9312563505f7cb1ff17761619d28922226986737
Thomas <the42ndfish@gmail.com>
2017-05-17 21:19:27 +1000

    IDE's not liking me

---
+++ DailyTask/Staircase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        void Staircase(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.WriteLine("#");
+                }
+            }
+        }
+        Staircase(7);
+        Staircase(3);
+    }
+}
